bddbedfb2586799a9d1f32b4b3ab5e2e4b50ee8a
Momchil Atanasov <momchil.dev@gmail.com>
2022-07-03 16:49:45 +0300

    Handle integer attribute formats

--- render/internal/vertex_array.go
+++ render/internal/vertex_array.go
@@ -16,8 +16,12 @@ func NewVertexArray(info render.VertexArrayInfo) *VertexArray {
 			wasmgl.BindBuffer(vertexBuffer.kind, vertexBuffer.raw)
 		}
 		wasmgl.EnableVertexAttribArray(attribute.Location)
-		count, compType, normalized := glAttribParams(attribute.Format)
-		wasmgl.VertexAttribPointer(attribute.Location, count, compType, normalized, binding.Stride, attribute.Offset)
+		count, compType, normalized, integer := glAttribParams(attribute.Format)
+		if integer {
+			wasmgl.VertexAttribIPointer(attribute.Location, count, compType, binding.Stride, attribute.Offset)
+		} else {
+			wasmgl.VertexAttribPointer(attribute.Location, count, compType, normalized, binding.Stride, attribute.Offset)
+		}
 	}
 	if indexBuffer, ok := info.IndexBuffer.(*Buffer); ok {
 		wasmgl.BindBuffer(indexBuffer.kind, indexBuffer.raw)
@@ -46,97 +50,106 @@ func (a *VertexArray) Release() {
 	a.id = 0
 }
 
-func glAttribParams(format render.VertexAttributeFormat) (int, int, bool) {
+func glAttribParams(format render.VertexAttributeFormat) (int, int, bool, bool) {
 	switch format {
 	case render.VertexAttributeFormatR32F:
-		return 1, wasmgl.FLOAT, false
+		return 1, wasmgl.FLOAT, false, false
 	case render.VertexAttributeFormatRG32F:
-		return 2, wasmgl.FLOAT, false
+		return 2, wasmgl.FLOAT, false, false
 	case render.VertexAttributeFormatRGB32F:
-		return 3, wasmgl.FLOAT, false
+		return 3, wasmgl.FLOAT, false, false
 	case render.VertexAttributeFormatRGBA32F:
-		return 4, wasmgl.FLOAT, false
+		return 4, wasmgl.FLOAT, false, false
 
 	case render.VertexAttributeFormatR16F:
-		return 1, wasmgl.HALF_FLOAT, false
+		return 1, wasmgl.HALF_FLOAT, false, false
 	case render.VertexAttributeFormatRG16F:
-		return 2, wasmgl.HALF_FLOAT, false
+		return 2, wasmgl.HALF_FLOAT, false, false
 	case render.VertexAttributeFormatRGB16F:
-		return 3, wasmgl.HALF_FLOAT, false
+		return 3, wasmgl.HALF_FLOAT, false, false
 	case render.VertexAttributeFormatRGBA16F:
-		return 4, wasmgl.HALF_FLOAT, false
+		return 4, wasmgl.HALF_FLOAT, false, false
 
 	case render.VertexAttributeFormatR16S:
-		return 1, wasmgl.SHORT, false
+		return 1, wasmgl.SHORT, false, false
 	case render.VertexAttributeFormatRG16S:
-		return 2, wasmgl.SHORT, false
+		return 2, wasmgl.SHORT, false, false
 	case render.VertexAttributeFormatRGB16S:
-		return 3, wasmgl.SHORT, false
+		return 3, wasmgl.SHORT, false, false
 	case render.VertexAttributeFormatRGBA16S:
-		return 4, wasmgl.SHORT, false
+		return 4, wasmgl.SHORT, false, false
 
 	case render.VertexAttributeFormatR16SN:
-		return 1, wasmgl.SHORT, true
+		return 1, wasmgl.SHORT, true, false
 	case render.VertexAttributeFormatRG16SN:
-		return 2, wasmgl.SHORT, true
+		return 2, wasmgl.SHORT, true, false
 	case render.VertexAttributeFormatRGB16SN:
-		return 3, wasmgl.SHORT, true
+		return 3, wasmgl.SHORT, true, false
 	case render.VertexAttributeFormatRGBA16SN:
-		return 4, wasmgl.SHORT, true
+		return 4, wasmgl.SHORT, true, false
 
 	case render.VertexAttributeFormatR16U:
-		return 1, wasmgl.UNSIGNED_SHORT, false
+		return 1, wasmgl.UNSIGNED_SHORT, false, false
 	case render.VertexAttributeFormatRG16U:
-		return 2, wasmgl.UNSIGNED_SHORT, false
+		return 2, wasmgl.UNSIGNED_SHORT, false, false
 	case render.VertexAttributeFormatRGB16U:
-		return 3, wasmgl.UNSIGNED_SHORT, false
+		return 3, wasmgl.UNSIGNED_SHORT, false, false
 	case render.VertexAttributeFormatRGBA16U:
-		return 4, wasmgl.UNSIGNED_SHORT, false
+		return 4, wasmgl.UNSIGNED_SHORT, false, false
 
 	case render.VertexAttributeFormatR16UN:
-		return 1, wasmgl.UNSIGNED_SHORT, true
+		return 1, wasmgl.UNSIGNED_SHORT, true, false
 	case render.VertexAttributeFormatRG16UN:
-		return 2, wasmgl.UNSIGNED_SHORT, true
+		return 2, wasmgl.UNSIGNED_SHORT, true, false
 	case render.VertexAttributeFormatRGB16UN:
-		return 3, wasmgl.UNSIGNED_SHORT, true
+		return 3, wasmgl.UNSIGNED_SHORT, true, false
 	case render.VertexAttributeFormatRGBA16UN:
-		return 4, wasmgl.UNSIGNED_SHORT, true
+		return 4, wasmgl.UNSIGNED_SHORT, true, false
 
 	case render.VertexAttributeFormatR8S:
-		return 1, wasmgl.BYTE, false
+		return 1, wasmgl.BYTE, false, false
 	case render.VertexAttributeFormatRG8S:
-		return 2, wasmgl.BYTE, false
+		return 2, wasmgl.BYTE, false, false
 	case render.VertexAttributeFormatRGB8S:
-		return 3, wasmgl.BYTE, false
+		return 3, wasmgl.BYTE, false, false
 	case render.VertexAttributeFormatRGBA8S:
-		return 4, wasmgl.BYTE, false
+		return 4, wasmgl.BYTE, false, false
 
 	case render.VertexAttributeFormatR8SN:
-		return 1, wasmgl.BYTE, true
+		return 1, wasmgl.BYTE, true, false
 	case render.VertexAttributeFormatRG8SN:
-		return 2, wasmgl.BYTE, true
+		return 2, wasmgl.BYTE, true, false
 	case render.VertexAttributeFormatRGB8SN:
-		return 3, wasmgl.BYTE, true
+		return 3, wasmgl.BYTE, true, false
 	case render.VertexAttributeFormatRGBA8SN:
-		return 4, wasmgl.BYTE, true
+		return 4, wasmgl.BYTE, true, false
 
 	case render.VertexAttributeFormatR8U:
-		return 1, wasmgl.UNSIGNED_BYTE, false
+		return 1, wasmgl.UNSIGNED_BYTE, false, false
 	case render.VertexAttributeFormatRG8U:
-		return 2, wasmgl.UNSIGNED_BYTE, false
+		return 2, wasmgl.UNSIGNED_BYTE, false, false
 	case render.VertexAttributeFormatRGB8U:
-		return 3, wasmgl.UNSIGNED_BYTE, false
+		return 3, wasmgl.UNSIGNED_BYTE, false, false
 	case render.VertexAttributeFormatRGBA8U:
-		return 4, wasmgl.UNSIGNED_BYTE, false
+		return 4, wasmgl.UNSIGNED_BYTE, false, false
 
 	case render.VertexAttributeFormatR8UN:
-		return 1, wasmgl.UNSIGNED_BYTE, true
+		return 1, wasmgl.UNSIGNED_BYTE, true, false
 	case render.VertexAttributeFormatRG8UN:
-		return 2, wasmgl.UNSIGNED_BYTE, true
+		return 2, wasmgl.UNSIGNED_BYTE, true, false
 	case render.VertexAttributeFormatRGB8UN:
-		return 3, wasmgl.UNSIGNED_BYTE, true
+		return 3, wasmgl.UNSIGNED_BYTE, true, false
 	case render.VertexAttributeFormatRGBA8UN:
-		return 4, wasmgl.UNSIGNED_BYTE, true
+		return 4, wasmgl.UNSIGNED_BYTE, true, false
+
+	case render.VertexAttributeFormatR8IU:
+		return 1, wasmgl.UNSIGNED_BYTE, false, true
+	case render.VertexAttributeFormatRG8IU:
+		return 2, wasmgl.UNSIGNED_BYTE, false, true
+	case render.VertexAttributeFormatRGB8IU:
+		return 3, wasmgl.UNSIGNED_BYTE, false, true
+	case render.VertexAttributeFormatRGBA8IU:
+		return 4, wasmgl.UNSIGNED_BYTE, false, true
 
 	default:
 		panic(fmt.Errorf("unknown attribute format: %d", format))
