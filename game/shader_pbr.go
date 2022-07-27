1bc495ae7cf986ec4d5bd6c1ffd56a5068c854ec
Momchil Atanasov <momchil.dev@gmail.com>
2022-07-28 00:29:49 +0300

    Initial support for bones

--- game/shader_pbr.go
+++ game/shader_pbr.go
@@ -18,6 +18,10 @@ func newPBRShaderSet(definition graphics.PBRMaterialDefinition) graphics.ShaderS
 		vsBuilder.AddFeature("USES_ALPHA_TEST")
 		fsBuilder.AddFeature("USES_ALPHA_TEST")
 	}
+	if definition.Armature {
+		vsBuilder.AddFeature("USES_BONES")
+		fsBuilder.AddFeature("USES_BONES")
+	}
 	return graphics.ShaderSet{
 		VertexShader:   vsBuilder.Build,
 		FragmentShader: fsBuilder.Build,
@@ -29,6 +33,10 @@ layout(location = 0) in vec4 coordIn;
 layout(location = 1) in vec3 normalIn;
 #if defined(USES_TEX_COORD0)
 layout(location = 3) in vec2 texCoordIn;
+#endif
+#if defined(USES_BONES)
+layout(location = 5) in vec4 weightsIn;
+layout(location = 6) in uvec4 jointsIn;
 #endif
 
 layout (std140) uniform Camera
@@ -53,9 +61,28 @@ void main()
 #if defined(USES_TEX_COORD0)
 	texCoordInOut = texCoordIn;
 #endif
+#if defined(USES_BONES)
+	mat4 modelMatrixA = modelMatrixIn[0] * modelMatrixIn[jointsIn.x+uint(1)];
+	mat4 modelMatrixB = modelMatrixIn[0] * modelMatrixIn[jointsIn.y+uint(1)];
+	mat4 modelMatrixC = modelMatrixIn[0] * modelMatrixIn[jointsIn.z+uint(1)];
+	mat4 modelMatrixD = modelMatrixIn[0] * modelMatrixIn[jointsIn.w+uint(1)];
+	vec4 worldPosition =
+		modelMatrixA * (weightsIn.x * coordIn) +
+		modelMatrixB * (weightsIn.y * coordIn) +
+		modelMatrixC * (weightsIn.z * coordIn) +
+		modelMatrixD * (weightsIn.w * coordIn);
+	vec3 worldNormal =
+		inverse(transpose(mat3(modelMatrixA))) * (weightsIn.x * normalIn) +
+		inverse(transpose(mat3(modelMatrixB))) * (weightsIn.y * normalIn) +
+		inverse(transpose(mat3(modelMatrixC))) * (weightsIn.z * normalIn) +
+		inverse(transpose(mat3(modelMatrixD))) * (weightsIn.w * normalIn);
+#else
 	mat4 modelMatrix = modelMatrixIn[gl_InstanceID];
-	normalInOut = inverse(transpose(mat3(modelMatrix))) * normalIn;
-	gl_Position = projectionMatrixIn * (viewMatrixIn * (modelMatrix * coordIn));
+	vec4 worldPosition = modelMatrix * coordIn;
+	vec3 worldNormal = inverse(transpose(mat3(modelMatrix))) * normalIn;
+#endif
+	normalInOut = worldNormal;
+	gl_Position = projectionMatrixIn * (viewMatrixIn * worldPosition);
 }
 `
 
