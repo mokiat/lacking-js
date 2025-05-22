04600954b5c95ed18eae8a0ffaf9c1dcf3bfddc9
Momchil Atanasov <momchil.dev@gmail.com>
2025-05-22 15:36:18 +0300

    Switch to translator from lacking

--- internal/shader/game/shadow.frag.glsl
+++
@@ -1,9 +0,0 @@
-/* template "version.glsl" */
-/* template "precision.glsl" */
-
-void main()
-{
-  // We don't render anything.
-
-  // FIXME: This does not handle alpha tested materials correctly.
-}
